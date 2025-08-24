9dab247df0db1c1d9f9d8f173a5666c5b688a8c9
KaileyLiou <kaileyliou09@gmail.com>
2025-08-24 17:12:38 -0400

    added content to home screen

--- My App/ContentView.swift
+++ My App/ContentView.swift
@@ -9,12 +9,42 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Text("Today")
-                .font(.title)
-                .fontWeight(.bold)
-        }
-        .padding()
+        NavigationStack {
+            ScrollView {
+                VStack(alignment: .leading, spacing: 20) {
+                    Text("Next Reminder")
+                        .font(.title2)
+                        .fontWeight(.bold)
+                    Text("HPV Vaccine – Aug 27, 2025 at 10:00 AM")
+                        .padding()
+                        .background(Color.gray.opacity(0.2))
+                        .cornerRadius(10)
+                    
+                    Text("Daily Health Tip")
+                        .font(.title2)
+                        .fontWeight(.bold)
+                    Text("Did you know? The HPV vaccine can prevent 90% of cervical cancers.")
+                        .padding()
+                        .background(Color.gray.opacity(0.2))
+                        .cornerRadius(10)
+                    
+
+                    Text("Daily Cybersecurity Tip")
+                        .font(.title2)
+                        .bold()
+                    Text("Never post your vaccine card or appointment screenshot online.")
+                        .padding()
+                        .background(Color.gray.opacity(0.2))
+                        .cornerRadius(10)
+                    
+                    Spacer()
+                    
+                } // end of vstack
+                .padding()
+                .navigationTitle("Screen+Secure")
+            } // end of scrollview
+        } // end of nav stack
+
     }
 }
 
